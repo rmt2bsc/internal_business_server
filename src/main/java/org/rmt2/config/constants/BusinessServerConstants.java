434da54c695f729c5d92c9d731dd2dbc5b5fde95
roy terrell <rmt2bsc@gmail.com>
2017-04-14 17:13:41 -0500

    Added message driven bean for Media project

--- src/main/java/org/rmt2/config/constants/BusinessServerConstants.java
+++ src/main/java/org/rmt2/config/constants/BusinessServerConstants.java
@@ -5,4 +5,5 @@ public class BusinessServerConstants {
     public static final String JMS_DEST_CONTACT_QUEUE = "rmt2.queue.addressbook";
     public static final String JMS_DEST_ACCOUNTING_QUEUE = "rmt2.queue.accounting";
     public static final String JMS_DEST_PROJECTTRACKER_QUEUE = "rmt2.queue.projecttracker";
+    public static final String JMS_DEST_MEDIA_QUEUE = "rmt2.queue.media";
 }
---
+++ src/main/java/org/rmt2/listeners/mdb/MediaBean.java
@@ -0,0 +1,27 @@
+package org.rmt2.listeners.mdb;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.MessageListener;
+
+import org.rmt2.config.constants.BusinessServerConstants;
+import org.rmt2.handlers.AbstractMessageDrivenBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Message-Driven Bean implementation class for: MediaBean
+ */
+@MessageDriven(
+        activationConfig = {
+                @ActivationConfigProperty(propertyName = "destination",
+                        propertyValue = BusinessServerConstants.JMS_DEST_MEDIA_QUEUE),
+                @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue") },
+        mappedName = BusinessServerConstants.JMS_DEST_MEDIA_QUEUE)
+public class MediaBean extends AbstractMessageDrivenBean implements MessageListener {
+    private static Logger logger = LoggerFactory.getLogger(MediaBean.class);
+
+    public MediaBean() {
+        logger.info(MediaBean.class.getSimpleName() + " was created successfully");
+    }
+}
