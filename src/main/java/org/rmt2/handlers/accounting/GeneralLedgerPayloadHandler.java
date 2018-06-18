71775786bbaf6af82549f2a079cbc51a8ac252a2
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2018-06-18 14:17:32 -0500

    IS-2: Refactored class, AccountingPayloadHandler, to GeneralLedgerPayloadHandler.

--- src/main/java/org/rmt2/handlers/accounting/GeneralLedgerPayloadHandler.java
+++ src/main/java/org/rmt2/handlers/accounting/GeneralLedgerPayloadHandler.java
@@ -1,22 +1,51 @@
 package org.rmt2.handlers.accounting;
 
 import org.rmt2.handlers.AbstractMessageHandler;
+import org.rmt2.handlers.InvalidRequestException;
+import org.rmt2.jaxb.AccountingGeneralLedgerRequest;
+import org.rmt2.jaxb.AccountingGeneralLedgerResponse;
+import org.rmt2.jaxb.GlDetailGroup;
+import org.rmt2.jaxb.ReplyStatusType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * @author appdev
+ * 
+ * @author roy.terrell
  *
  */
-public class AccountingPayloadHandler extends AbstractMessageHandler {
-    private static final Logger logger = LoggerFactory.getLogger(AccountingPayloadHandler.class);
+public class GeneralLedgerPayloadHandler extends
+        AbstractMessageHandler<AccountingGeneralLedgerRequest, AccountingGeneralLedgerResponse, GlDetailGroup> {
+    private static final Logger logger = LoggerFactory.getLogger(GeneralLedgerPayloadHandler.class);
 
     /**
      * @param payload
      */
-    public AccountingPayloadHandler() {
+    public GeneralLedgerPayloadHandler() {
         super();
-        logger.info(AccountingPayloadHandler.class.getName() + " was instantiated successfully");
+        this.responseObj = jaxbObjFactory.createAccountingGeneralLedgerResponse();
+        logger.info(GeneralLedgerPayloadHandler.class.getName() + " was instantiated successfully");
+    }
+
+    @Override
+    protected void validdateRequest(AccountingGeneralLedgerRequest req) throws InvalidRequestException {
+        if (req == null) {
+            throw new InvalidRequestException("Accounting General Ledger message request element is invalid");
+        }
+    }
+
+    @Override
+    protected String buildResponse(GlDetailGroup payload, ReplyStatusType replyStatus) {
+        if (replyStatus != null) {
+            this.responseObj.setReplyStatus(replyStatus);    
+        }
+        
+        if (payload != null) {
+            this.responseObj.getProfile().add(payload);
+        }
+        
+        String xml = this.jaxb.marshalMessage(this.responseObj);
+        return xml;
     }
 
     // /*
