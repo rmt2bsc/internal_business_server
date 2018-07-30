e7cf6f7fa20743763bd63c6d06f6a0da5d12c836
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2018-07-29 20:32:07 -0500

    IS-22: Added JUnit test cases representing  GET_ITEM_STATUS_HIST, GET_CURRENT_ITEM_STATUS_HIST, and GET_ITEM_MASTER transaction codes

--- src/test/java/org/rmt2/AccountingMockData.java
+++ src/test/java/org/rmt2/AccountingMockData.java
@@ -1,15 +1,24 @@
 package org.rmt2;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.dao.mapping.orm.rmt2.GlAccountCategory;
 import org.dao.mapping.orm.rmt2.GlAccountTypes;
 import org.dao.mapping.orm.rmt2.GlAccounts;
+import org.dao.mapping.orm.rmt2.ItemMaster;
+import org.dao.mapping.orm.rmt2.ItemMasterStatusHist;
 import org.dto.AccountCategoryDto;
 import org.dto.AccountDto;
 import org.dto.AccountTypeDto;
+import org.dto.ItemMasterDto;
+import org.dto.ItemMasterStatusHistDto;
 import org.dto.adapter.orm.account.generalledger.Rmt2AccountDtoFactory;
+import org.dto.adapter.orm.inventory.Rmt2InventoryDtoFactory;
+
+import com.SystemException;
+import com.api.util.RMT2Date;
 
 public class AccountingMockData {
 
@@ -148,4 +157,141 @@ public class AccountingMockData {
        return orm;
    }
 
+   
+   /**
+    * 
+    * @param id
+    * @param itemTypeId
+    * @param serialNo
+    * @param vendorItemNo
+    * @param creditorId
+    * @param description
+    * @param qty
+    * @param unitCost
+    * @param reatilPrice
+    * @param active
+    * @return
+    */
+   public static final ItemMaster createMockOrmItemMaster(int id,
+           int itemTypeId, String serialNo, String vendorItemNo,
+           int creditorId, String description, int qty, double unitCost,
+           boolean active) {
+       ItemMaster i = new ItemMaster();
+       i.setItemId(id);
+       i.setItemTypeId(itemTypeId);
+       i.setItemSerialNo(serialNo);
+       i.setVendorItemNo(vendorItemNo);
+       i.setCreditorId(creditorId);
+       i.setDescription(description);
+       i.setQtyOnHand(qty);
+       i.setUnitCost(unitCost);
+       i.setActive(active ? 1 : 0);
+       i.setOverrideRetail(0);
+       i.setMarkup(5);
+       i.setRetailPrice((qty * unitCost) * i.getMarkup());
+       return i;
+   }
+   
+   public static final List<ItemMasterDto> createMockItemMasterList() {
+       List<ItemMasterDto> list = new ArrayList<>();
+       ItemMaster o = AccountingMockData.createMockOrmItemMaster(100, 1,
+               "100-111-111", "11111110", 1351, "Item1", 1, 1.23, true);
+       ItemMasterDto p = Rmt2InventoryDtoFactory.createItemMasterInstance(o);
+       list.add(p);
+
+       o = AccountingMockData.createMockOrmItemMaster(101, 1,
+               "101-111-111", "11111111", 1352, "Item2", 2, 1.23, true);
+       p = Rmt2InventoryDtoFactory.createItemMasterInstance(o);
+       list.add(p);
+
+       o = AccountingMockData.createMockOrmItemMaster(102, 1,
+               "102-111-111", "11111112", 1353, "Item3", 3, 1.23, true);
+       p = Rmt2InventoryDtoFactory.createItemMasterInstance(o);
+       list.add(p);
+
+       o = AccountingMockData.createMockOrmItemMaster(103, 1,
+               "103-111-111", "11111113", 1354, "Item4", 4, 1.23, true);
+       p = Rmt2InventoryDtoFactory.createItemMasterInstance(o);
+       list.add(p);
+
+       o = AccountingMockData.createMockOrmItemMaster(104, 1,
+               "104-111-111", "11111114", 1355, "Item5", 5, 1.23, true);
+       p = Rmt2InventoryDtoFactory.createItemMasterInstance(o);
+       list.add(p);
+       return list;
+   }
+   
+   /**
+    * 
+    * @param id
+    * @param itemId
+    * @param statusId
+    * @param unitCost
+    * @param markup
+    * @param effDate
+    * @param endDate
+    * @param reason
+    * @return
+    */
+   public static final ItemMasterStatusHist createMockOrmItemMasterStatusHistory(
+           int id, int itemId, int statusId, double unitCost, double markup,
+           String effDate, String endDate, String reason) {
+       ItemMasterStatusHist i = new ItemMasterStatusHist();
+       i.setItemStatusHistId(id);
+       i.setItemId(itemId);
+       i.setItemStatusId(statusId);
+       i.setUnitCost(unitCost);
+       i.setMarkup(markup);
+       try {
+           i.setEffectiveDate(RMT2Date.stringToDate(effDate));
+       } catch (SystemException e) {
+           i.setEffectiveDate(new Date());
+       }
+       try {
+           i.setEndDate(RMT2Date.stringToDate(endDate));
+       } catch (SystemException e) {
+           i.setEndDate(new Date());
+       }
+       i.setReason(reason);
+       return i;
+   }
+
+   
+   /**
+    * 
+    * @return
+    */
+   public static final List<ItemMasterStatusHistDto> createMockItemStatusHistoryList() {
+       List<ItemMasterStatusHistDto> list = new ArrayList<ItemMasterStatusHistDto>();
+       ItemMasterStatusHist o = AccountingMockData.createMockOrmItemMasterStatusHistory(10, 100, 1000, 12.50, 3,
+                       "2017-01-01", "2017-03-01",
+                       "Item Status History Description 1");
+       ItemMasterStatusHistDto p = Rmt2InventoryDtoFactory.createItemStatusHistoryInstance(o);
+       list.add(p);
+
+       o = AccountingMockData.createMockOrmItemMasterStatusHistory(11,
+               101, 1001, 13.50, 3, "2017-01-02", "2017-03-02",
+               "Item Status History Description 2");
+       p = Rmt2InventoryDtoFactory.createItemStatusHistoryInstance(o);
+       list.add(p);
+
+       o = AccountingMockData.createMockOrmItemMasterStatusHistory(12,
+               102, 1002, 14.50, 3, "2017-01-03", "2017-03-03",
+               "Item Status History Description 3");
+       p = Rmt2InventoryDtoFactory.createItemStatusHistoryInstance(o);
+       list.add(p);
+
+       o = AccountingMockData.createMockOrmItemMasterStatusHistory(13,
+               103, 1003, 15.50, 3, "2017-01-04", "2017-03-04",
+               "Item Status History Description 4");
+       p = Rmt2InventoryDtoFactory.createItemStatusHistoryInstance(o);
+       list.add(p);
+
+       o = AccountingMockData.createMockOrmItemMasterStatusHistory(14,
+               104, 1004, 16.50, 3, "2017-01-05", "2017-03-05",
+               "Item Status History Description 5");
+       p = Rmt2InventoryDtoFactory.createItemStatusHistoryInstance(o);
+       list.add(p);
+       return list;
+   }
 }
---
+++ src/test/java/org/rmt2/handler/accounting/InventoryItemStatusHistoryJmsTest.java
@@ -0,0 +1,115 @@
+package org.rmt2.handler.accounting;
+
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
+
+import java.util.List;
+
+import org.dto.ItemMasterStatusHistDto;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.modules.inventory.InventoryApi;
+import org.modules.inventory.InventoryApiException;
+import org.modules.inventory.InventoryApiFactory;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.rmt2.AccountingMockData;
+import org.rmt2.BaseMockMessageDrivenBeanTest;
+import org.rmt2.api.handlers.inventory.ItemStatusHistoryApiHandler;
+
+import com.api.messaging.jms.JmsClientManager;
+import com.api.util.RMT2File;
+
+
+
+/**
+ * Test the idenity and invocation of the GL Account API Message Handler.
+ * 
+ * @author appdev
+ *
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ JmsClientManager.class, ItemStatusHistoryApiHandler.class, InventoryApiFactory.class })
+public class InventoryItemStatusHistoryJmsTest extends BaseMockMessageDrivenBeanTest {
+
+    private static final String DESTINATION = "Test-Accounting-Queue";
+    private InventoryApiFactory mockApiFactory;
+    private InventoryApi mockApi;
+
+
+    /**
+     * 
+     */
+    public InventoryItemStatusHistoryJmsTest() {
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see testcases.messaging.MessageToListenerToHandlerTest#setUp()
+     */
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        this.mockApiFactory = Mockito.mock(InventoryApiFactory.class);
+        this.mockApi = Mockito.mock(InventoryApi.class);
+        try {
+            whenNew(InventoryApiFactory.class).withNoArguments().thenReturn(this.mockApiFactory);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        when(mockApiFactory.createApi(isA(String.class))).thenReturn(mockApi);
+        return;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see testcases.messaging.MessageToListenerToHandlerTest#tearDown()
+     */
+    @After
+    public void tearDown() throws Exception {
+        return;
+    }
+
+    @Test
+    public void invokeHandelrSuccess_Fetch() {
+        String request = RMT2File.getFileContentsAsString("xml/inventory/ItemStatusHistoryFetchRequest.xml");
+        List<ItemMasterStatusHistDto> mockDtoDataResponse = AccountingMockData.createMockItemStatusHistoryList();
+        this.setupMocks(DESTINATION, request);
+        try {
+            when(this.mockApi.getItemStatusHist(isA(ItemMasterStatusHistDto.class))).thenReturn(mockDtoDataResponse);
+        } catch (InventoryApiException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            this.startTest();    
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail("An unexpected exception was thrown");
+        }
+        
+    }
+    
+    @Test
+    public void invokeHandelrError_Fetch_Incorrect_Trans_Code() {
+        String request = RMT2File.getFileContentsAsString("xml/inventory/ItemStatusHistoryIncorrectTransCodeRequest.xml");
+        this.setupMocks(DESTINATION, request);
+        try {
+            this.startTest();    
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail("An unexpected exception was thrown");
+        }
+        
+    }
+}
