8575bf91392aceea043fb1b519b75ec2b9a6b141
appdev <royterrell2@gmail.com>
2020-03-27 18:09:23 -0500

    IS-41:  Prepared the address book related test request XML files to be integration test ready.

--- src/test/java/org/rmt2/handler/accounting/generalledger/GlAccountCategoryJmsTest.java
+++ src/test/java/org/rmt2/handler/accounting/generalledger/GlAccountCategoryJmsTest.java
@@ -37,7 +37,7 @@ import com.api.util.RMT2File;
 @PrepareForTest({ JmsClientManager.class, GlAccountCategoryApiHandler.class, GeneralLedgerApiFactory.class })
 public class GlAccountCategoryJmsTest extends BaseMockMessageDrivenBeanTest {
 
-    private static final String DESTINATION = "Test-Accounting-Queue";
+    private static final String DESTINATION = "rmt2.queue.accounting";
     private GeneralLedgerApiFactory mockApiFactory;
     private GlAccountApi mockApi;
 
