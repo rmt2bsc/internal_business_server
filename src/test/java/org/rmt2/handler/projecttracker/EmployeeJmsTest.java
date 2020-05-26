4deeaee2c28cbc636aa861cfff95df49d95dc718
appdev <royterrell2@gmail.com>
2020-05-25 21:09:48 -0500

    IS-42:  Created test cases for Employee Update message handler

--- src/test/java/org/rmt2/handler/projecttracker/EmployeeJmsTest.java
+++ src/test/java/org/rmt2/handler/projecttracker/EmployeeJmsTest.java
@@ -2,11 +2,13 @@ package org.rmt2.handler.projecttracker;
 
 import static org.mockito.Matchers.eq;
 import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
 
 import org.dto.EmployeeDto;
+import org.dto.PersonalContactDto;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -14,6 +16,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.modules.ProjectTrackerApiConst;
+import org.modules.contacts.ContactsApi;
+import org.modules.contacts.ContactsApiException;
+import org.modules.contacts.ContactsApiFactory;
 import org.modules.employee.EmployeeApi;
 import org.modules.employee.EmployeeApiException;
 import org.modules.employee.EmployeeApiFactory;
@@ -36,11 +41,14 @@ import com.api.util.RMT2File;
  *
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ EmployeeQueryApiHandler.class, JmsClientManager.class, EmployeeApiFactory.class })
+@PrepareForTest({ EmployeeQueryApiHandler.class, JmsClientManager.class, EmployeeApiFactory.class, ContactsApiFactory.class })
 public class EmployeeJmsTest extends BaseMockMessageDrivenBeanTest {
 
     private static final String DESTINATION = "rmt2.queue.projecttracker";
+    public static final int EMPLOYEE_ID = 2000;
+    public static final int CONTACT_ID = 900;
     private EmployeeApi mockApi;
+    private ContactsApi mockContactApi;
 
 
     /**
@@ -58,6 +66,12 @@ public class EmployeeJmsTest extends BaseMockMessageDrivenBeanTest {
     @Override
     public void setUp() throws Exception {
         super.setUp();
+
+        mockContactApi = Mockito.mock(ContactsApi.class);
+        PowerMockito.mockStatic(ContactsApiFactory.class);
+        when(ContactsApiFactory.createApi()).thenReturn(mockContactApi);
+        doNothing().when(this.mockContactApi).close();
+
         this.mockApi = Mockito.mock(EmployeeApi.class);
         PowerMockito.mockStatic(EmployeeApiFactory.class);
         when(EmployeeApiFactory.createApi(eq(ProjectTrackerApiConst.APP_NAME))).thenReturn(this.mockApi);
@@ -97,6 +111,32 @@ public class EmployeeJmsTest extends BaseMockMessageDrivenBeanTest {
         
     }
     
+    @Test
+    public void invokeHandelrSuccess_Update() {
+        String request = RMT2File.getFileContentsAsString("xml/projecttracker/employee/EmployeeInsertRequest.xml");
+        this.setupMocks(DESTINATION, request);
+        try {
+            when(this.mockContactApi.updateContact(isA(PersonalContactDto.class))).thenReturn(CONTACT_ID);
+        } catch (ContactsApiException e) {
+            Assert.fail("Unable to setup mock stub for creating employee contact record");
+        }
+        try {
+            when(this.mockApi.update(isA(EmployeeDto.class))).thenReturn(EMPLOYEE_ID);
+        } catch (EmployeeApiException e) {
+            Assert.fail("Unable to setup mock stub for creating employee record");
+        }
+
+        try {
+            this.startTest();
+            Mockito.verify(this.mockContactApi).updateContact(isA(PersonalContactDto.class));
+            Mockito.verify(this.mockApi).update(isA(EmployeeDto.class));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail("An unexpected exception was thrown");
+        }
+
+    }
+
     @Test
     public void invokeHandelrError_Fetch_Incorrect_Trans_Code() {
         String request = RMT2File
