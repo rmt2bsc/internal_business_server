c911c88f940d45b29765e2d90df969dc87ff0b40
appdev <royterrell2@gmail.com>
2021-02-02 16:38:53 -0600

    IS-52:   Media content home application link changes -  Created constants for JMS client id constants for linking media content to the Accounting and ProjectTracker applications

--- src/main/java/org/rmt2/config/constants/BusinessServerConstants.java
+++ src/main/java/org/rmt2/config/constants/BusinessServerConstants.java
@@ -6,4 +6,9 @@ public class BusinessServerConstants {
     public static final String JMS_DEST_ACCOUNTING_QUEUE = "rmt2.queue.accounting";
     public static final String JMS_DEST_PROJECTTRACKER_QUEUE = "rmt2.queue.projecttracker";
     public static final String JMS_DEST_MEDIA_QUEUE = "rmt2.queue.media";
+    public static final String JMS_DEST_MEDIA_TOPIC = "rmt2.topic.media";
+
+    public static final String JMS_TOPIC_CLIENT_ID_ACCOUNTING_ATTACHMENT = "000000001-ACCT_ATTACH";
+    public static final String JMS_TOPIC_CLIENT_ID_PROJECTTRACKER_ATTACHMENT = "000000002-PROJ_ATTACH";
+
 }
