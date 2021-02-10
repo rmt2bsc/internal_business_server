c5b03a83ce7aa5d5ca4005a239b133096fa68f3e
appdev <royterrell2@gmail.com>
2021-02-10 16:16:34 -0600

    DEV:  Created unit test case for consolidated media query request using a single search term.

--- src/test/java/org/rmt2/handler/media/maint/ConsolidatedMediaJmsTest.java
+++ src/test/java/org/rmt2/handler/media/maint/ConsolidatedMediaJmsTest.java
@@ -21,6 +21,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import org.rmt2.api.handlers.maint.ConsolidatedMediaFetchApiHandler;
 import org.rmt2.handler.BaseMockSingleConsumerMDBTest;
 import org.rmt2.handler.media.MediaJmsMockDtoFactory;
+import org.rmt2.handler.media.MediaJmsMockOrmFactory;
 
 import com.api.messaging.jms.JmsClientManager;
 import com.api.util.RMT2File;
@@ -96,4 +97,47 @@ public class ConsolidatedMediaJmsTest extends BaseMockSingleConsumerMDBTest {
         
     }
 
+    @Test
+    public void invokeHandelrSuccess_Fetch_SearchTerm() {
+        String request = RMT2File.getFileContentsAsString("xml/media/maint/ConsolidateMediaWithSingleSearchTermQueryRequest.xml");
+        List<VwArtistDto> mockListData = MediaJmsMockDtoFactory.createConsolidatedMediaDistinctMockData();
+
+        // Modify data set for testing purposes
+        mockListData.get(0).setArtistName("After 7");
+
+        mockListData.get(1).setArtistId(MediaJmsMockOrmFactory.TEST_ARTIST_ID + 1);
+        mockListData.get(1).setProjectId(MediaJmsMockOrmFactory.TEST_PROJECT_ID +
+                1);
+        mockListData.get(1).setArtistName("Afterlife");
+
+        mockListData.get(2).setArtistId(MediaJmsMockOrmFactory.TEST_ARTIST_ID + 2);
+        mockListData.get(2).setProjectId(MediaJmsMockOrmFactory.TEST_PROJECT_ID +
+                2);
+        mockListData.get(2).setProjectName("Afterlife Again");
+
+        mockListData.get(3).setArtistId(MediaJmsMockOrmFactory.TEST_ARTIST_ID + 3);
+        mockListData.get(3).setProjectId(MediaJmsMockOrmFactory.TEST_PROJECT_ID +
+                3);
+        mockListData.get(3).setTrackName("After The Love Is Gone");
+
+        mockListData.get(4).setArtistId(MediaJmsMockOrmFactory.TEST_ARTIST_ID + 4);
+        mockListData.get(4).setProjectId(MediaJmsMockOrmFactory.TEST_PROJECT_ID +
+                4);
+        mockListData.get(4).setTrackName("After The Storm");
+
+        this.setupMocks(DESTINATION, request);
+        try {
+            when(this.mockApi.getConsolidatedArtist(isA(VwArtistDto.class))).thenReturn(mockListData);
+        } catch (AudioVideoApiException e) {
+            Assert.fail("Unable to setup mock stub for fetching vw_audio_video_artist records");
+        }
+
+        try {
+            this.startTest();
+            Mockito.verify(this.mockApi).getConsolidatedArtist(isA(VwArtistDto.class));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail("An unexpected exception was thrown");
+        }
+    }
 }
