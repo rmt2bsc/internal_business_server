ac03d9b6456fa4aec3d31a51e8b78c06dd68a7c2
appdev <royterrell2@gmail.com>
2021-03-02 13:12:43 -0600

    IS-57:  Created unit test for transaction code, DELETE_RESOURCE_SUB_TYPE

--- src/test/java/org/rmt2/handler/authentication/admin/ResourceSubTypeJmsTest.java
+++ src/test/java/org/rmt2/handler/authentication/admin/ResourceSubTypeJmsTest.java
@@ -112,4 +112,24 @@ public class ResourceSubTypeJmsTest extends BaseMockSingleConsumerMDBTest {
             Assert.fail("An unexpected exception was thrown");
         }
     }
+
+    @Test
+    public void invokeHandelrSuccess_Delete() {
+        String request = RMT2File.getFileContentsAsString("xml/authentication/admin/ResourceSubTypeDeleteRequest.xml");
+        this.setupMocks(DESTINATION, request);
+        try {
+            when(this.mockApi.deleteResourceSubType(isA(ResourceDto.class))).thenReturn(1);
+        } catch (ResourceRegistryApiException e) {
+            e.printStackTrace();
+            Assert.fail("Resource Sub Type update test case failed");
+        }
+
+        try {
+            this.startTest();
+            Mockito.verify(this.mockApi).deleteResourceSubType(isA(ResourceDto.class));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail("An unexpected exception was thrown");
+        }
+    }
 }
