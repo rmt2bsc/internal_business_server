5b23bfc355e9f7b959a3f95c8edfb7fa635c094a
appdev <royterrell2@gmail.com>
2021-03-20 12:01:26 -0500

    IS-60:  Developed Unit test for transaction code, UPDATE_USER_GROUP

---
+++ src/test/java/org/rmt2/handler/authentication/admin/UserGroupJmsTest.java
@@ -0,0 +1,94 @@
+package org.rmt2.handler.authentication.admin;
+
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+import org.dto.UserDto;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.modules.users.UserApi;
+import org.modules.users.UserApiException;
+import org.modules.users.UserApiFactory;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.rmt2.handler.BaseMockSingleConsumerMDBTest;
+
+import com.api.messaging.jms.JmsClientManager;
+import com.api.util.RMT2File;
+
+
+
+/**
+ * Test the idenity and invocation of the user group related JMS messages for
+ * the Authentication API Message Handler.
+ * 
+ * @author appdev
+ *
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ JmsClientManager.class, UserApiFactory.class })
+public class UserGroupJmsTest extends BaseMockSingleConsumerMDBTest {
+
+    private static final String DESTINATION = "rmt2.queue.authentication";
+    private UserApi mockApi;
+
+
+    /**
+     * 
+     */
+    public UserGroupJmsTest() {
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see testcases.messaging.MessageToListenerToHandlerTest#setUp()
+     */
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        mockApi = Mockito.mock(UserApi.class);
+        PowerMockito.mockStatic(UserApiFactory.class);
+        when(UserApiFactory.createApiInstance()).thenReturn(mockApi);
+        doNothing().when(this.mockApi).close();
+        return;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see testcases.messaging.MessageToListenerToHandlerTest#tearDown()
+     */
+    @After
+    public void tearDown() throws Exception {
+        return;
+    }
+
+    @Test
+    public void invokeHandelrSuccess_Update() {
+        String request = RMT2File.getFileContentsAsString("xml/authentication/admin/UserGroupUpdateRequest.xml");
+        this.setupMocks(DESTINATION, request);
+        try {
+            when(this.mockApi.updateGroup(isA(UserDto.class))).thenReturn(1);
+        } catch (UserApiException e) {
+            e.printStackTrace();
+            Assert.fail("User Group update test case failed");
+        }
+
+        try {
+            this.startTest();
+            Mockito.verify(this.mockApi).updateGroup(isA(UserDto.class));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail("An unexpected exception was thrown");
+        }
+    }
+
+}
