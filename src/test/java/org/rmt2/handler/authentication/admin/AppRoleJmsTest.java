740c9a1a97e9561121405dd5b5c4c841ebd96ec0
appdev <royterrell2@gmail.com>
2021-04-02 13:13:51 -0500

    dev:  Developed unit test case for transaction code, UPDATE_APP_ROLE

---
+++ src/test/java/org/rmt2/handler/authentication/admin/AppRoleJmsTest.java
@@ -0,0 +1,93 @@
+package org.rmt2.handler.authentication.admin;
+
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+import org.dto.CategoryDto;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.modules.SecurityModuleException;
+import org.modules.roles.AppRoleApi;
+import org.modules.roles.RoleSecurityApiFactory;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.rmt2.handler.BaseMockSingleConsumerMDBTest;
+
+import com.api.messaging.jms.JmsClientManager;
+import com.api.util.RMT2File;
+
+
+
+/**
+ * Test the idenity and invocation of the application role related JMS messages
+ * for the Authentication API Message Handler.
+ * 
+ * @author appdev
+ *
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ JmsClientManager.class, RoleSecurityApiFactory.class })
+public class AppRoleJmsTest extends BaseMockSingleConsumerMDBTest {
+
+    private static final String DESTINATION = "rmt2.queue.authentication";
+    private AppRoleApi mockApi;
+
+
+    /**
+     * 
+     */
+    public AppRoleJmsTest() {
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see testcases.messaging.MessageToListenerToHandlerTest#setUp()
+     */
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        mockApi = Mockito.mock(AppRoleApi.class);
+        PowerMockito.mockStatic(RoleSecurityApiFactory.class);
+        when(RoleSecurityApiFactory.createAppRoleApi()).thenReturn(mockApi);
+        doNothing().when(this.mockApi).close();
+        return;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see testcases.messaging.MessageToListenerToHandlerTest#tearDown()
+     */
+    @After
+    public void tearDown() throws Exception {
+        return;
+    }
+
+    @Test
+    public void invokeHandelrSuccess_Update() {
+        String request = RMT2File.getFileContentsAsString("xml/authentication/admin/AppRoleUpdateRequest.xml");
+        this.setupMocks(DESTINATION, request);
+        try {
+            when(this.mockApi.update(isA(CategoryDto.class))).thenReturn(1);
+        } catch (SecurityModuleException e) {
+            e.printStackTrace();
+            Assert.fail("User Group update test case failed");
+        }
+
+        try {
+            this.startTest();
+            Mockito.verify(this.mockApi).update(isA(CategoryDto.class));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail("An unexpected exception was thrown");
+        }
+    }
+}
